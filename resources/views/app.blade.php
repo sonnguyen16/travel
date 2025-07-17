661fb6425636e05b57e136e1ee3a80de430f63d4
sonnguyen16 <sonnv2212@gmail.com>
2025-07-17 11:21:17 +0700

    update

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,7 +7,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="icon" href="{{ asset('images/ico.png') }}" type="image/x-icon">
 
-    <title inertia>{{ config('app.name', 'Laravel') }}</title>
+    <title inertia>Dalattourist®</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
